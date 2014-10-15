fb448c01534e58c05a0372e33aec42a166125d23
Max Gaukler <development@maxgaukler.de>
2014-10-15 18:53:28 +0200

    add known_hosts

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # needs to be run from main directory
-sftp -i deployment/deployment_private_key.id_rsa -b deployment/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
+sftp -i deployment/deployment_private_key.id_rsa -oUserKnownHostsFile=deployment/known_hosts -oPasswordAuthentication=no -b deployment/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
