57053880ad60b1210b4a115fe292d05e47f6cee3
Max Gaukler <development@maxgaukler.de>
2014-10-15 22:14:24 +0200

    fix makefile

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+deploydir="$(dirname $0)"
 # needs to be run from main directory
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]];  then
 	# read variables $encrypted_....._key and _iv, the "..." part is set with ENCYRPTION_VAR_NAME
@@ -10,7 +11,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$TRAVIS_BRANCH" == "master" ]]
 	openssl aes-256-cbc -K $key -iv $iv \
 		-in deployment/deployment_private_key.id_rsa.enc -out deployment/deployment_private_key.id_rsa -d
 	chmod 600 deployment/deployment_private_key.id_rsa
-	sftp -i deployment/deployment_private_key.id_rsa -oUserKnownHostsFile=deployment/known_hosts -oPasswordAuthentication=no -b deployment/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
+	sftp -i deployment/deployment_private_key.id_rsa -oUserKnownHostsFile=$deploydir/known_hosts -oPasswordAuthentication=no -b $deploydir/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
 else
 	echo "skipping deployment: branch $TRAVIS_BRANCH pullrequest $TRAVIS_PULL_REQUEST"
 fi
