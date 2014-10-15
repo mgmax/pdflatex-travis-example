be6385c63c86b78e78214068e0b8e392325bf4fb
Max Gaukler <development@maxgaukler.de>
2014-10-15 19:10:01 +0200

    fix known_hosts

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 # needs to be run from main directory
-sftp -i deployment/deployment_private_key.id_rsa -oUserKnownHostsFile=deployment/known_hosts -oPasswordAuthentication=no -b deployment/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
+if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master"]];  then
+	sftp -i deployment/deployment_private_key.id_rsa -oUserKnownHostsFile=deployment/known_hosts -oPasswordAuthentication=no -b deployment/sftp_batch buildserver-uploads@macgyver.fablab.fau.de
+else
+	echo "skipping deployment: branch $TRAVIS_BRANCH pullrequest $TRAVIS_PULL_REQUEST"
+fi
